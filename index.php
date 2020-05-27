6f0e8e9e942dd8cb324a4b7ade72aa719e5ad0f4
dragan.ribes <dragan.ribes@epita.fr>
2020-05-28 00:45:41 +0200

    Index & Stylesheet

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Accueil</title>
+</head>
+<body>
+	<!-- PROUT SUPERSONIQUE a toi mon ami !-->
+</body>
+</html>
