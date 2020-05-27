308e381d318d85aad5bd7826ff0b393084d01957
dragan.ribes <dragan.ribes@epita.fr>
2020-05-28 00:55:02 +0200

    Base

--- index.php
+++ index.php
@@ -7,5 +7,12 @@
 </head>
 <body>
 	<!-- PROUT SUPERSONIQUE a toi mon ami !-->
+	<!-- AJOUTER ICI LA NAVIGATION -->
+	<article>
+		<!-- TEXTE D'EXEMPLE, a rendre modifiable par les admins par la suite ! -->
+		Voici le site internet de « Roliste Universe »,<br> 
+		Ce serveur est une communauté de personnes regroupées autour de leur passion commune, le jeu de rôle. Que ce soit du RP ou du jeu de rôle à proprement parler, ce serveur vous proposera plusieurs univers passant par toutes les époques et tous les styles de mondes. En plus de faire du RP et d’organiser des parties de jeu de rôle en vocal via discord ou encore en IRL, le serveur soutient une partie hors-jeu active et sympathique. Il arrive de temps en temps que nous organisions des événements externes au jeu de rôle comme des rencontres IRL des membres ou des sorties diverses et variées ! Il est très simple de rejoindre les mondes proposés et de participer à la vie de ces mondes ou à la vie du serveur en général. Vous pouvez même proposer votre monde en tant que MJ ! Le serveur ouvre ses portes à tous et à toutes ! <br>
+		A bientôt dans la taverne la plus proche de votre univers !
+	</article>
 </body>
 </html>
