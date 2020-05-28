9fb2ec21df31ad51196176ae1622cfe529895a2e
Dragan Ribes <dragan.ribes@gmail.com>
2020-05-29 00:05:49 +0200

    font update + title

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RU_Website
 The repository for Roliste Universe Website
+
+Ce projet comprends un background fait par : lorentatelma#0486
+Le logo appartient à : Fridris#0782
+Nous utilisons Spectrashell, police libre de droit de Fontspring.com
---
+++ nav.php
@@ -0,0 +1,9 @@
+<a href="index.php" id="linkToMenu"><header><span id="title">
+	<img src="images/logo-serv.gif" id="logo" />
+	Roliste Universe
+</span></header></a>
+<nav>
+	<div>Le serveur</div>
+	<div>JDR et RP</div>
+	<div>Contact</div>
+</nav>
